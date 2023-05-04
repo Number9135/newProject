a2d13351db9457194f18ae67ac56b950c35f8475
number9135 <navigate0425@gmail.com>
2023-05-04 17:31:50 +0900

    DibListPage

--- secondaryProject/src/components/pages/DibsListPage.js
+++ secondaryProject/src/components/pages/DibsListPage.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import { Button,Avatar, Menu, Divider, Provider,Card, Text, FAB, Portal } from 'react-native-paper';
+import data from '../../../data.json';
+import { EvilIcons, Entypo } from '@expo/vector-icons';
 
-export default function DibsListPage() {
+
+export default function DibsListPage(content) {
   return (
 
-    <View>
-        <Text>
-            asdasdsadasdasd
-        </Text>
+    <View style = {{flex:1, borderWidth:1, justifyContent:'center', alignItems:'center'}}>
+
+        <ScrollView style={{borderWidth:1, width:400, }}>
+        
+        <View style={{borderWidth: 1, borderRadius:20, height: 200, width: 380, marginTop: 10, alignSelf:'center'}}>
+
+        </View>
+
+        </ScrollView>
+       
     </View>
 
     )
