fe1680ab9ef58d350c1989160dd05184a022abac
number9135 <navigate0425@gmail.com>
2023-05-06 17:28:32 +0900

    DibListPage

--- secondaryProject/src/components/pages/DibsListPage.js
+++ secondaryProject/src/components/pages/DibsListPage.js
@@ -1,25 +1,145 @@
 import React, { useEffect, useState } from 'react';
-import {View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
-import { Button,Avatar, Menu, Divider, Provider,Card, Text, FAB, Portal } from 'react-native-paper';
-import data from '../../../data.json';
-import { EvilIcons, Entypo } from '@expo/vector-icons';
+import {View, StyleSheet, Image, TouchableOpacity, Text, ScrollView} from 'react-native';
+import { Provider } from 'react-native-paper';
+import data from '../../../data.json'
+import CateCard from '../form/CateCard';
 import { firebase_db } from '../../../firebaseConfig';
-import LikePage from '../pages/LikePage';
-import {fetchData} from '../pages/LikePage'
 
 
-export default function DibsListPage() {
+export default function DibListPage() {
+  
+  const [stateMap,setStateMap] = useState([])
+  const [cateState,setCateState] = useState([])
+
+  const [index, setIndex] = React.useState(0);
+
+  let like = data.like
+
+  
+  useEffect(()=>{
 
    
-  
+      firebase_db.ref('/like').once('value').then((snapshot)=>{
+        console.log('파이어베이스에서 데이터 가져왔습니다!')
+        let like = snapshot.val();
+        setStateMap(data.like)
+        setCateState(data.like)
+      })
+    
+  },[like])
  
-    return (
-    <View style ={{flex:1, borderWidth:1, paddingTop:50,}}>
-    <Text>dibList:</Text>
-      <View>{fetchData}</View>
-    
-    </View>
-    );
-    }
 
-    
+  return (
+    <Provider>
+   
+      
+      <ScrollView style={styles.middleContainer}>
+        {
+          like.map((content, i)=>{
+            return(
+              <Card content = {content} key = {i}/>
+                
+            )
+          })
+        }
+      </ScrollView>
+
+      
+    </Provider>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        width : 410,
+        height : 700,
+        borderWidth : 1,
+       
+    },
+
+    headerContainer : {
+      borderWidth : 1,
+      height : 150,
+      justifyContent : "center",
+      alignItems : "center",
+     
+  },
+  
+  headerImageStyle : {
+      height : 100,
+      width : 405,
+  },
+  
+  headerImageContainer : {
+      borderWidth : 1,
+      height : 100,
+      backgroundColor : 'white'
+     
+  },
+  
+  upperContainer : {
+      borderWidth : 1,
+      height : 50,
+      width : 410,
+      flexDirection : "row",
+      alignContent : "flex-start",
+      backgroundColor : 'white'
+      
+  },
+  
+  uppercontainer01 : {
+      
+      borderWidth : 0,
+      height : 50,
+      width : 50,
+      alignItems : "center",
+      justifyContent : "center",
+  },
+  
+  upperContainer02 : {
+      
+      borderWidth : 0,
+      height : 50,
+      width : 310,
+      marginTop : 0,
+  },
+
+  titleText : {
+      paddingTop : 20,
+      fontSize : 20,
+  },
+
+
+
+  middleContainer : {
+    borderWidth : 0,
+    height : 330,
+    alignSelf : 'center',
+    width : 410,
+    paddingLeft : 25,
+   
+
+  },
+
+
+
+  lowerContainer : {
+    borderWidth : 1,
+    flex : 1,
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+  },
+
+  buttonStyle : {
+    borderWidth : 1,
+    height : 50,
+    width : 60,
+    borderRadius : 10,
+    alignSelf : 'center',
+    justifyContent : 'center',
+    alignItems : 'center',
+  }
+
+})
